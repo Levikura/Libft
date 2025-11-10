e2fe186dfcb0cf629e7471af2e857b1a77595241
Lukas Frank <lufrank@3-G-6.42heilbronn.de>
2025-11-10 05:59:28 +0100

    libft change

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lufrank <lufrank@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 22:36:31 by lufrank           #+#    #+#             */
-/*   Updated: 2025/11/10 02:50:27 by lufrank          ###   ########.fr       */
+/*   Updated: 2025/11/10 05:56:11 by lufrank          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,4 +49,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 #endif
